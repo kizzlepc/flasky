381a612a773f51ef3c7e1006aa63396e01df6099
kizzle <kizzle@qq.com>
2017-05-10 17:37:07 +0800

    test ^_^

--- hello.py
+++ hello.py
@@ -1,25 +1,18 @@
-from flask import Flask,make_response,redirect,abort
-from flask.ext.script import Manager
+from flask import Flask,render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello,World!</h1>'
-
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello,%s!</h1>'%name
-
-
-@app.route('/user/<ids>')
-def get_user(ids):
-    user = load_user(ids)
-    if not user:
-        abort(404)
-    return '<h1>Hello,%s!</h1>'%user.name
+    return render_template('user.html',name=name)
 
 if __name__ == '__main__':
     #app.run(debug = True)
