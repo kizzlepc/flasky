7c61d92a1389b28f94c1b11e1029feeee01b6cb3
kizzle <kizzle@qq.com>
2017-05-11 01:34:31 +0800

    Four chapter over

--- hello.py
+++ hello.py
@@ -1,18 +1,44 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY']='hard to guess string'
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('what is your name?',validators=[Required()])
+    submit=SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
 
 if __name__ == '__main__':
     #app.run(debug = True)
